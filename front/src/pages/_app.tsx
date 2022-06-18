0f3d8a8f6d882814e59806b507157fa1034ca309
Daaiki <daaikii@outlook.jp>
2022-06-19 01:49:13 +0900

    認証情報を確認する

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,16 +1,56 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { RecoilRoot, useSetRecoilState } from 'recoil'
 import { ChakraProvider } from '@chakra-ui/react'
 import { customTheme } from '@/theme'
 import { Layout } from '@/components/Layout'
 import '@/lib/firebase'
+import { currentUserState } from '@/recoil/atoms'
+
+const AppInit = () => {
+  const setCurrentUser = useSetRecoilState(currentUserState)
+  const router = useRouter()
+
+  const checkAuth = async () => {
+    try {
+      const auth = getAuth()
+      await onAuthStateChanged(auth, (currentUser) => {
+        if (currentUser) {
+          setCurrentUser({
+            uid: currentUser.uid,
+            isAnonymous: currentUser.isAnonymous
+          })
+        } else {
+          setCurrentUser(null)
+          router.push('/')
+        }
+      })
+    } catch {
+      setCurrentUser(null)
+      await router.push('/')
+    }
+  }
+
+  useEffect(() => {
+    checkAuth()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.pathname])
+
+  return null
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={customTheme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={customTheme}>
+        <Layout>
+          <Component {...pageProps} />
+          <AppInit />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
   )
 }
 
