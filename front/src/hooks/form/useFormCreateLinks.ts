72190452a0f892a840867ed6e3901042206b2350
Daaiki <daaikii@outlook.jp>
2022-07-06 13:38:00 +0900

    修正

--- front/src/hooks/form/useFormCreateLinks.ts
+++ front/src/hooks/form/useFormCreateLinks.ts
@@ -16,7 +16,7 @@ const schema = yup.object({
   date: yup
     .string()
     .matches(/^[0-9]{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])$/, {
-      message: 'yyyy/mm/ddの形式で入力してください。(ex: 2022/06/21)'
+      message: 'yyyy/mm/ddの形式で入力してください(ex: 2022/06/21)'
     }),
   links: yup.array().of(
     yup.object().shape({
