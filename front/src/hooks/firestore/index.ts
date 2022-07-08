f4dd4d2ef3b92e2ef4bc2e2bea55f7719f23681c
Daaiki <daaikii@outlook.jp>
2022-07-09 00:57:10 +0900

    format

--- front/src/hooks/firestore/index.ts
+++ front/src/hooks/firestore/index.ts
@@ -1,2 +1,3 @@
 // travelink
-export { useGetTravelink, usePostTravelink } from '@/hooks/firestore/travelink'
+export { useGetTravelink } from '@/hooks/firestore/travelink/useGetTravelink'
+export { usePostTravelink } from '@/hooks/firestore/travelink/usePostTravelink'
--- front/src/hooks/firestore/travelink/index.ts
+++
@@ -1,2 +0,0 @@
-export { useGetTravelink } from '@/hooks/firestore/travelink/useGetTravelink'
-export { usePostTravelink } from '@/hooks/firestore/travelink/usePostTravelink'
