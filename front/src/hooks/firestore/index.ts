8feb72906ea60c042f1738a088d03809c6a52a6d
whooolemilk <weekender.nyanko@gmail.com>
2022-07-10 12:55:32 +0900

    hooks/firestore/profile作成

--- front/src/hooks/firestore/index.ts
+++ front/src/hooks/firestore/index.ts
@@ -2,4 +2,4 @@
 export { useGetTravelink } from '@/hooks/firestore/travelink/useGetTravelink'
 export { useGetTravelinkList } from '@/hooks/firestore/travelink/useGetTravelinkList'
 export { useCreateTravelink } from '@/hooks/firestore/travelink/useCreateTravelink'
-export { useGetOwnerProfile } from '@/hooks/firestore/travelink/useGetOwnerProfile'
+export { useGetOwnerProfile } from '@/hooks/firestore/profile/useGetOwnerProfile'
