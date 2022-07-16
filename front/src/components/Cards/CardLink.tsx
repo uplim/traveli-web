4ddc0886b641e88d704ae9cf4868d6f18b527f1a
Ibuki <64850924+eve00@users.noreply.github.com>
2022-07-16 19:26:25 +0900

    🥐CardLinkコンポーネントを作成 (#87)
    * テストページ
    
    * CardLinkを作成
    
    * やーんふぉーまっと
    
    * やーんふぉーまっと２
    
    * 文字がはみ出たら...に置き換え
    
    Co-authored-by: whooolemilk <weekender.nyanko@gmail.com>
    Co-authored-by: Miyuusa <74092547+whooolemilk@users.noreply.github.com>

---
+++ front/src/components/Cards/CardLink.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+
+type CardLinkProps = {
+  icon: string
+  label: string
+  url: string
+}
+
+export const CardLink = ({ label, url }: CardLinkProps) => {
+  return (
+    <Link href={url}>
+      <Flex
+        position={'relative'}
+        top={0}
+        left={0}
+        w={'100%'}
+        h={'5.2rem'}
+        borderRadius={'10rem'}
+        bgColor={'white'}
+        filter={'drop-shadow(4px 4px 10px #E4EBEE)'}
+        align={'center'}
+      >
+        {/* TODO: アイコンPropsの受け取り */}
+        <Box
+          backgroundColor={'gray'}
+          borderRadius={'100%'}
+          w={'3.8rem'}
+          h={'3.8rem'}
+          marginLeft={'0.7rem'}
+        />
+        <Text
+          w={'80%'}
+          textAlign={'center'}
+          fontSize={'md'}
+          overflow={'hidden'}
+          whiteSpace={'nowrap'}
+          textOverflow={'ellipsis'}
+          margin={'0 1.6rem'}
+        >
+          {label}
+        </Text>
+      </Flex>
+    </Link>
+  )
+}
