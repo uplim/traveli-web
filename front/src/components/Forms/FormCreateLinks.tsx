ba408389f7d8f1614e591cc697505701a5a2e744
ibuki <64850924+eve00@users.noreply.github.com>
2022-07-17 13:45:35 +0900

    コンフリクト修正

--- front/src/components/Forms/FormCreateLinks.tsx
+++ front/src/components/Forms/FormCreateLinks.tsx
@@ -28,6 +28,7 @@ export const FormCreateLinks = () => {
 
   return (
     <Box>
+
       <FormControl isInvalid={errors.title ? true : false}>
         <FormLabel>旅の名前</FormLabel>
         <Input
@@ -38,6 +39,7 @@ export const FormCreateLinks = () => {
           {errors.title && errors.title.message}
         </FormErrorMessage>
       </FormControl>
+      
       <FormControl isInvalid={errors.date ? true : false}>
         <FormLabel>日時（任意）</FormLabel>
         <Input placeholder={'2022/01/23 ~ 2022/01/25'} {...register('date')} />
@@ -45,16 +47,19 @@ export const FormCreateLinks = () => {
           {errors.date && errors.date.message}
         </FormErrorMessage>
       </FormControl>
+
       <FormControl>
         <FormLabel>サムネイル画像（任意）</FormLabel>
         <Input type={'file'} accept="image/*" onChange={handleChangeImage} />
         <Image alt={''} src={image ? URL.createObjectURL(image) : ''} />
       </FormControl>
+
       <FormControl>
         <List>
           {fields.map((item, index) => {
             return (
               <ListItem key={item.id}>
+
                 <FormControl isInvalid={!!errors.links}>
                   <FormLabel>URL</FormLabel>
                   <Input
@@ -66,6 +71,7 @@ export const FormCreateLinks = () => {
                       errors.links?.[index].url?.message}
                   </FormErrorMessage>
                 </FormControl>
+                
                 <FormLabel>ラベル</FormLabel>
                 <Input {...register(`links.${index}.label`)} />
               </ListItem>
@@ -100,6 +106,7 @@ export const FormCreateLinks = () => {
           送信する
         </Button>
       </FormControl>
+      
     </Box>
   )
 }
