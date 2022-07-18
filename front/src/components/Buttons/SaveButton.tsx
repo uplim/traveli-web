ee7ddc4bfbb44038b79168c273e2126b22d8d4cf
Daaiki <daaikii@outlook.jp>
2022-07-18 20:06:30 +0900

    押したときに保存済みになるように

--- front/src/components/Buttons/SaveButton.tsx
+++ front/src/components/Buttons/SaveButton.tsx
@@ -1,7 +1,7 @@
 import { useUpdateBookmark } from '@/hooks/firestore'
 import { currentUserState } from '@/recoil/atoms'
 import { TravelinkRequestType } from '@/types/db'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text, useBoolean } from '@chakra-ui/react'
 import { useRecoilValue } from 'recoil'
 import { IconSave } from '@/components/Icons'
 
@@ -11,17 +11,23 @@ type SaveButtonProps = {
 }
 
 export const SaveButton = ({ data, isChecked }: SaveButtonProps) => {
+  const [saved, setSaved] = useBoolean()
   const updateBookmark = useUpdateBookmark
   const currentUser = useRecoilValue(currentUserState)
+
+  const onClick = (data: TravelinkRequestType, uid: string) => {
+    updateBookmark(data, uid)
+    setSaved.on()
+  }
 
   return (
     <Box
       onClick={() =>
-        !isChecked && currentUser && updateBookmark(data, currentUser.uid)
+        !isChecked && currentUser && onClick(data, currentUser.uid)
       }
     >
       <IconSave w={'2.5rem'} h={'2.5rem'} margin={'0 auto'} />
-      <Text>{isChecked ? '保存済み' : '保存'}</Text>
+      <Text>{isChecked || saved ? '保存済み' : '保存'}</Text>
     </Box>
   )
 }
