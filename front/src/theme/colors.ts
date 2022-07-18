3c0dd820a8e4eb1c73ebc042b7f692356453893f
ibuki <64850924+eve00@users.noreply.github.com>
2022-07-19 00:16:43 +0900

    switch専用カラー　brandBlue追加

--- front/src/theme/colors.ts
+++ front/src/theme/colors.ts
@@ -3,6 +3,9 @@ export const colors = {
   base: '#F1F4FF',
   logoBlack: '#393849',
   brandBlue: '#1A489D',
+  'brandBlue-switch': {
+    500: '#1A489D'
+  },
   gray: '#7E7E7E',
   black: '#252525',
   brandPink: {
