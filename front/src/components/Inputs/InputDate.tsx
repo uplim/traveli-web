aa7c2d509751253392404fe65934d70a1d497881
ibuki <64850924+eve00@users.noreply.github.com>
2022-07-23 20:28:41 +0900

    日時を日本語で表示

--- front/src/components/Inputs/InputDate.tsx
+++ front/src/components/Inputs/InputDate.tsx
@@ -1,12 +1,15 @@
 import 'react-datepicker/dist/react-datepicker.css'
 import React from 'react'
 import { Control, Controller, Path } from 'react-hook-form'
-import DatePicker from 'react-datepicker'
+import DatePicker, { registerLocale } from 'react-datepicker'
+import { ja } from 'date-fns/locale'
 
 type Props<T> = {
   name: Path<T>
   control: Control<T>
 }
+
+registerLocale('ja', ja)
 
 export const InputDate = <T,>({ name, control }: Props<T>) => {
   return (
@@ -16,14 +19,15 @@ export const InputDate = <T,>({ name, control }: Props<T>) => {
       render={({ field: { onChange, value = [null, null] } }) => {
         const v = value as [Date | null, Date | null]
         return (
-          <DatePicker
-            selectsRange={true}
-            dateFormat={'yyyy/MM/dd'}
-            onChange={onChange}
-            startDate={v[0]}
-            endDate={v[1]}
-            withPortal
-          />
+            <DatePicker
+              locale={ja}
+              selectsRange={true}
+              dateFormat={'yyyy/MM/dd'}
+              onChange={onChange}
+              startDate={v[0]}
+              endDate={v[1]}
+              withPortal
+            />
         )
       }}
     />
