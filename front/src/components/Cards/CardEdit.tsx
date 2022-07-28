f5d8200de0113a09cc8722dd4bf4857879bcc9da
ibuki <64850924+eve00@users.noreply.github.com>
2022-07-28 18:16:23 +0900

    yarn format

--- front/src/components/Cards/CardEdit.tsx
+++ front/src/components/Cards/CardEdit.tsx
@@ -121,10 +121,7 @@ export const CardEdit = ({
                   h={'1.68rem'}
                   marginRight={'1.6rem'}
                 />
-                <Box
-                  fontSize={'sm'}
-                  color={'#FF4D4D'}
-                >
+                <Box fontSize={'sm'} color={'#FF4D4D'}>
                   削除する
                 </Box>
               </MenuItem>
