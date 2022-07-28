8b867fbcb38803c5005c7a20dea26bc7853de988
ibuki <64850924+eve00@users.noreply.github.com>
2022-07-28 19:10:31 +0900

    commit

--- front/src/components/Cards/index.ts
+++ front/src/components/Cards/index.ts
@@ -1,3 +1,4 @@
 export { CardLink } from '@/components/Cards/CardLink'
 export { CardEdit } from '@/components/Cards/CardEdit'
 export { CardEditWrapper } from '@/components/Cards/CardEditWrapper'
+
