1b9d5a177c6da842fb2277c45f1166aa06b2595b
Daaiki <daaikii@outlook.jp>
2022-07-29 13:22:06 +0900

    型にcategory追加

--- front/src/types/db/index.ts
+++ front/src/types/db/index.ts
@@ -1,3 +1,3 @@
 export type { CurrentUser, Profile } from '@/types/db/user'
-export type { TravelinkRequestType } from '@/types/db/travelink'
+export type { TravelinkRequestType, CategoryType } from '@/types/db/travelink'
 export type { BookmarkType } from '@/types/db/bookmark'
