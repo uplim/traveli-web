dcddac100ad4c5ba7a40201150dfcfbeb7300648
Daaiki <daaikii@outlook.jp>
2022-08-02 04:05:27 +0900

    UI修正

--- front/src/components/Cards/CardHome.tsx
+++ front/src/components/Cards/CardHome.tsx
@@ -1,9 +1,8 @@
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
-import { Avatar, Box, Flex, Heading, Spacer, Link } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Heading, Link } from '@chakra-ui/react'
 import { BookmarkType, TravelinkRequestType } from '@/types/db'
 import { format } from 'date-fns'
-import { IconLink } from '@/components/Icons'
 import { Button } from '@/components/Buttons'
 
 type CardHomeProps = {
@@ -33,42 +32,49 @@ export const CardHome = ({ data }: CardHomeProps) => {
           h={'12.9rem'}
           borderTopRadius={'10'}
         />
-        <Box
-          paddingTop={'1.1rem'}
-          paddingLeft={'1.8rem'}
-          paddingRight={'1.8rem'}
-          borderBottomRadius={'10'}
-        >
-          {data.date[0] && (
-            <Box fontSize={'xs'} color={'gray'}>
-              {data.date[0] && format(data.date[0].toDate(), 'yyyy/MM/dd')}~
-              {data.date[1] && format(data.date[1].toDate(), 'yyyy/MM/dd')}
-            </Box>
-          )}
-          <Box>
-            <Heading
-              paddingTop={'0.6rem'}
-              paddingBottom={'1.4rem'}
-              fontSize={'lg'}
-            >
-              {data.title}
-            </Heading>
-          </Box>
-          <Flex alignContent={'baseline'}>
-            <Flex>
-              <Avatar w={'2.4rem'} h={'2.4rem'} src={data.ownerIcon} />
 
-              <Box paddingLeft={'1rem'} fontSize={'md'}>
-                {data.ownerName}
+        <Flex justify={'space-between'}>
+          <Box
+            paddingTop={'1.1rem'}
+            paddingLeft={'1.8rem'}
+            paddingRight={'1.8rem'}
+            borderBottomRadius={'10'}
+          >
+            {data.date[0] && (
+              <Box fontSize={'xs'} color={'gray'}>
+                {data.date[0] && format(data.date[0].toDate(), 'yyyy/MM/dd')}~
+                {data.date[1] && format(data.date[1].toDate(), 'yyyy/MM/dd')}
               </Box>
-            </Flex>
+            )}
+            <Box>
+              <Heading
+                paddingTop={'0.6rem'}
+                paddingBottom={'1.4rem'}
+                fontSize={'lg'}
+              >
+                {data.title}
+              </Heading>
+            </Box>
+            <Flex alignContent={'baseline'}>
+              <Flex>
+                <Avatar w={'2.4rem'} h={'2.4rem'} src={data.ownerIcon} />
 
-            <Spacer />
-            <Button>
-              <IconLink w={'2rem'} h={'2rem'} color={'gray'} />
-            </Button>
-          </Flex>
-        </Box>
+                <Box paddingLeft={'1rem'} fontSize={'md'}>
+                  {data.ownerName}
+                </Box>
+              </Flex>
+            </Flex>
+          </Box>
+          <Button mt={'1.6rem'}>
+            <Flex w={'3.6rem'} h={'3.6rem'} align={'center'} justify={'center'}>
+              <Box
+                bgImage={'/images/icons/homeMenu.svg'}
+                w={'2.4rem'}
+                h={'2.4rem'}
+              />
+            </Flex>
+          </Button>
+        </Flex>
       </Link>
     </NextLink>
   )
