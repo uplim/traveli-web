6f6f3249c2ee233c5ab4ba1e31b7ba5e31831f05
ibuki <64850924+eve00@users.noreply.github.com>
2022-09-07 14:35:46 +0900

    CardLinkのラベルを編集できるように

--- front/src/stories/components/Cards/CardLink.stories.tsx
+++ front/src/stories/components/Cards/CardLink.stories.tsx
@@ -6,24 +6,27 @@ export default {
   title: 'components/Cards/CardLink'
 } as Meta
 
-const Template: Story = ({ category }) => (
+const Template: Story = (args) => (
   <Box w={'100%'} bgColor={'base'}>
     <Box as={'main'} w={'container.sm'} p={'1.6rem'} h={'100vh'}>
-      <CardLink label={'カードリンク'} category={category} />
+      <CardLink label={args.label} category={args.category} />
     </Box>
   </Box>
 )
 
 export const Others = Template.bind({})
 Others.args = {
+  label:'リンク',
   category: 'その他'
 }
 
 export const Food = Template.bind({})
 Food.args = {
+  label:'リンク',
   category: '食事'
 }
 export const Place = Template.bind({})
 Place.args = {
+  label:'リンク',
   category: '場所'
 }
