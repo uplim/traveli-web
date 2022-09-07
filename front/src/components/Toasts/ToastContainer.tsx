0d866f50ae731ed2b352b5107e05a6cae2e39624
ibuki <64850924+eve00@users.noreply.github.com>
2022-09-07 15:23:50 +0900

    ToastContainerコンポーネントを切る

---
+++ front/src/components/Toasts/ToastContainer.tsx
@@ -0,0 +1,15 @@
+import { ToastContainer } from 'react-toastify'
+
+
+export const BaseToastContainer = () => {
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={true}
+        closeButton={false}
+      />
+    </>
+  )
+}
