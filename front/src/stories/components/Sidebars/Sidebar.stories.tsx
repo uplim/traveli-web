77ac5ef006e517763121188cba5b9b14ceffef1a
ibuki <64850924+eve00@users.noreply.github.com>
2022-09-11 12:54:06 +0900

    Sidebarの制御を外に出す

---
+++ front/src/stories/components/Sidebars/Sidebar.stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react'
+import { Sidebar } from '@/components/Sidebars'
+import { Box } from '@chakra-ui/react'
+
+export default {
+  title: 'components/Sidebars/Sidebar'
+} as Meta
+const Template: Story = (args) => {
+  return (
+    <Box w={'100%'} bgColor={'base'}>
+      <Box as={'main'} w={'container.sm'} p={'1.6rem'} h={'100vh'}>
+        <Sidebar isOpen={true} onClose={() => {}} icon={'/images/dummy.png'} name={args.name} />
+      </Box>
+    </Box>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  name: 'とらべり　太郎'
+}
