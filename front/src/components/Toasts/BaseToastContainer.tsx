45ab30ed18591e8773964b6c27011195abd41ae4
ibuki <64850924+eve00@users.noreply.github.com>
2022-09-11 21:12:03 +0900

    run formatter

--- front/src/components/Toasts/BaseToastContainer.tsx
+++ front/src/components/Toasts/BaseToastContainer.tsx
@@ -2,11 +2,11 @@ import { ToastContainer } from 'react-toastify'
 
 export const BaseToastContainer = () => {
   return (
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={true}
-        closeButton={false}
-      />
+    <ToastContainer
+      position="top-center"
+      autoClose={2000}
+      hideProgressBar={true}
+      closeButton={false}
+    />
   )
 }
