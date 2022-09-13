f08452f80f0888579324957dce4e1ddb31a2809e
Daiki Maekawa <daaikii@outlook.jp>
2022-09-13 23:48:56 +0900

    snapshotテスト導入

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,20 +1,21 @@
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  // next.config.jsとテスト環境用の.envファイルが配置されたディレクトリをセット。基本は"./"で良い。
   dir: "./",
 });
 
-// Jestのカスタム設定を設置する場所。従来のプロパティはここで定義。
 const customJestConfig = {
-  // jest.setup.jsを作成する場合のみ定義。
-  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    // aliasを定義 （tsconfig.jsonのcompilerOptions>pathsの定義に合わせる）
-    "^@/components/(.*)$": "<rootDir>/components/$1",
-    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
   testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest"
+  }
 };
 
 // createJestConfigを定義することによって、本ファイルで定義された設定がNext.jsの設定に反映されます
